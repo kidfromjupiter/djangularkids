e33319e1f91cd28371dccb08b75a200e7f52e941
kidfromjupiter <limahaliyana@gmail.com>
2021-11-16 12:53:45 +0530

    cleaned up the folder structure

--- chordsapp_backend/chords_config/settings.py
+++ chordsapp_backend/chords_config/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'chordsapp.urls'
+ROOT_URLCONF = 'chords_config.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'chordsapp.wsgi.application'
+WSGI_APPLICATION = 'chords_config.wsgi.application'
 
 
 # Database
