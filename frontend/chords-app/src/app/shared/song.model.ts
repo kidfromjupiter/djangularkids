daacaa84899c500c0c115389f1612e09532a6a26
tkmiro <mirko.tkacik@gmail.com>
2021-11-19 14:29:42 +0100

    changed Song model from Class to interface

--- frontend/chords-app/src/app/shared/song.model.ts
+++ frontend/chords-app/src/app/shared/song.model.ts
@@ -1,16 +1,7 @@
-export class Song {
+export interface Song {
     id:string;
     title:string;
     artist:string;
     lyrics:string;
     date_of_creation:string;
-    
-    constructor(id:string, title:string, artist:string, lyrics:string, date_of_creation:string){
-        this.id = id;
-        this.title = title;
-        this.artist = artist;
-        this.lyrics = lyrics;
-        this.date_of_creation = date_of_creation;
-    }
-
 }
