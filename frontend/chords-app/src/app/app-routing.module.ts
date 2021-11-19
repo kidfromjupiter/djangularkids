2c81efe8259e356a632e9ce6961c818ba052739c
tkmiro <mirko.tkacik@gmail.com>
2021-11-19 08:22:58 +0100

    deleted routes to testing components test and second

--- frontend/chords-app/src/app/app-routing.module.ts
+++ frontend/chords-app/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from './test/test.component';
-import { SecondComponent } from './second/second.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AboutComponent } from './about/about.component';
 import { SongListComponent } from './song-list/song-list.component';
@@ -10,8 +8,6 @@ import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path:"", component:WelcomeComponent},
-  { path: 'test', component: TestComponent },
-  {path: 'second', component: SecondComponent },
   {path: 'about', component: AboutComponent},
   {path: 'songlist', component: SongListComponent},
   {path: '**', component: PageNotFoundComponent}
