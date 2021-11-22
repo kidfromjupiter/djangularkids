70ec33d184d8957cc130fec40c9ed9d0a9d62129
tkmiro <mirko.tkacik@gmail.com>
2021-11-22 12:54:47 +0100

    edited styling of song-overview component

--- frontend/chords-app/src/app/song-list/song-list.component.ts
+++ frontend/chords-app/src/app/song-list/song-list.component.ts
@@ -11,8 +11,7 @@ import { Song } from '../shared/song.model';
 export class SongListComponent implements OnInit {
   songs: Song[] = [
     {id: "1", artist: "Paul McCartney", title: "Yesterday", lyrics: "bla", date_of_creation:"12"},
-    {id: "2141", artist: "The Beatles", title: "All my loving", lyrics: "Lorem ipsum", date_of_creation:"15.7.1985"}
-  ];
+    {id: "2141", artist: "The Beatles", title: "All my loving", lyrics: "Close your eyes and I'll kiss you Tomorrow I'll miss you Remember I'll always be true And then while I'm away, I'll write home everyday And I'll send all my loving to you", date_of_creation:"15.7.1985"}];
   constructor(private apiHttpService: ApiHttpService, private songsService: SongsService) { }
 
   ngOnInit(): void {
